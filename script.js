b0e020bec03cfcc33e9d43ef4fd4edd946ef1581
kennisjoseph <kennisjoseph5@gmail.com>
2021-11-26 19:33:15 +0530

    This is a short project made with html, css, and Javascript.

---
+++ script.js
@@ -0,0 +1,201 @@
+const popupbox1 = document.querySelector('.popupbox1');
+const popupbox2 = document.querySelector('.popupbox2');
+const popupbox3 = document.querySelector('.popupbox3');
+const popupbox4 = document.querySelector('.popupbox4');
+const popupbox5 = document.querySelector('.popupbox5');
+const popupbox6 = document.querySelector('.popupbox6');
+const popupbox7 = document.querySelector('.popupbox7');
+const popupbox8 = document.querySelector('.popupbox8');
+const popupbox9 = document.querySelector('.popupbox9');
+const popupbox10 = document.querySelector('.popupbox10');
+
+function pop1() {
+    if (popupbox1.style.display == "none") {
+        popupbox1.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox1.style.display = "none";
+    }
+}
+pop1();
+
+function pop2() {
+    if (popupbox2.style.display == "none") {
+        popupbox2.style.display = "flex";
+        popupbox1.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox2.style.display = "none";
+    }
+}
+pop2();
+
+function pop3() {
+    if (popupbox3.style.display == "none") {
+        popupbox3.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox1.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox3.style.display = "none";
+    }
+}
+pop3();
+
+function pop4() {
+    if (popupbox4.style.display == "none") {
+        popupbox4.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox1.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox4.style.display = "none";
+    }
+}
+pop4();
+
+function pop5() {
+    if (popupbox5.style.display == "none") {
+        popupbox5.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox1.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox5.style.display = "none";
+    }
+}
+pop5();
+
+function pop6() {
+    if (popupbox6.style.display == "none") {
+        popupbox6.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox1.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox6.style.display = "none";
+    }
+}
+pop6();
+
+function pop7() {
+    if (popupbox7.style.display == "none") {
+        popupbox7.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox1.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox7.style.display = "none";
+    }
+}
+pop7();
+
+function pop8() {
+    if (popupbox8.style.display == "none") {
+        popupbox8.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox1.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox8.style.display = "none";
+    }
+}
+pop8();
+
+function pop9() {
+    if (popupbox9.style.display == "none") {
+        popupbox9.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox1.style.display = "none";
+        popupbox10.style.display = "none";
+    }
+    else {
+        popupbox9.style.display = "none";
+    }
+}
+pop9();
+
+function pop10() {
+    if (popupbox10.style.display == "none") {
+        popupbox10.style.display = "flex";
+        popupbox2.style.display = "none";
+        popupbox3.style.display = "none";
+        popupbox4.style.display = "none";
+        popupbox5.style.display = "none";
+        popupbox6.style.display = "none";
+        popupbox7.style.display = "none";
+        popupbox8.style.display = "none";
+        popupbox9.style.display = "none";
+        popupbox1.style.display = "none";
+    }
+    else {
+        popupbox10.style.display = "none";
+    }
+}
+pop10();
+
